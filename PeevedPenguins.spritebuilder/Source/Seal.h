da36ca4ed01103068d951c0e7a9c2bda55b4007b
dftikas <dftikas@gmail.com>
2014-11-21 19:49:17 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Δημήτρης Φτίκας on 21/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
