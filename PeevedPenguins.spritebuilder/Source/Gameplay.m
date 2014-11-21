acf4d07ba6a33ed71356e46fc50addd0dea87626
dftikas <dftikas@gmail.com>
2014-11-22 01:08:07 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -19,9 +19,12 @@
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    // visualize physics bodies & joints
+    _physicsNode.debugDraw = TRUE;
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    
 }
 
 // called on every touch in this scene
